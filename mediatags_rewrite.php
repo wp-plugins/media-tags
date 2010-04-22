c6dd61bae8266f8b2ed8ad15835574cb18abf921
pmenard <pmenard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-22 14:57:27 +0000

    tagging version 2.2.7
    git-svn-id: https://plugins.svn.wordpress.org/media-tags/tags/2.2.7@232033 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mediatags_rewrite.php
+++ mediatags_rewrite.php
@@ -34,6 +34,7 @@ function mediatags_parseQuery() {
 	
 	if (is_MEDIA_TAGS_URL()) {
 		global $wp_query;
+			
 		$wp_query->is_single = false;
 		$wp_query->is_page = false;
 		$wp_query->is_archive = false;
@@ -42,6 +43,8 @@ function mediatags_parseQuery() {
 		$wp_query->is_404 = false;
 
 		$wp_query->is_mediatags = true;
+
+		//echo "wp_query<pre>"; print_r($wp_query); echo "</pre>";
 
 		add_action('template_redirect', 'mediatags_includeTemplate');
 	}	
@@ -57,7 +60,7 @@ function is_MEDIA_TAGS_URL() {
 		&& ($wp_version >= 2.0) ) ? get_query_var(MEDIA_TAGS_QUERYVAR) : $GLOBALS[MEDIA_TAGS_QUERYVAR];
 
 	//$series = get_query_var(SERIES_QUERYVAR);
-	if ( (!is_null($MEDIA_TAGS_URL) && ($MEDIA_TAGS_URL != '')) || $wp_query->is_mediatags == true)
+	if ( (!is_null($MEDIA_TAGS_URL) && ($MEDIA_TAGS_URL != '')) || ((isset($wp_query->is_mediatags)) && ($wp_query->is_mediatags == true)) )
 		return true;
 	else
 		return false;
@@ -68,20 +71,54 @@ function mediatags_includeTemplate() {
 		$template = '';
 					
 		$mediatag_var = get_query_var(MEDIA_TAGS_QUERYVAR);
+		//echo "mediatag_var=[".$mediatag_var."]<br />";
+
+		$mediatag_feed_var = get_query_var('feed');
+		//echo "mediatag_feed_var=[".$mediatag_feed_var."]<br />";
+
 		if ($mediatag_var)
 		{	
 			$mediatag_term = is_term( $mediatag_var, MEDIA_TAGS_TAXONOMY );
 			if ($mediatag_term)
 			{					
-				$fname_parts = pathinfo(MEDIA_TAGS_TEMPLATE);
-				if (strlen($fname_parts['filename']))
+				if (($mediatag_feed_var == "rss")
+ 				 || ($mediatag_feed_var == "rss2")
+				 || ($mediatag_feed_var == "feed"))
 				{
-					$template_filename = TEMPLATEPATH. "/" . 
+					//load_template( ABSPATH . WPINC . '/feed-rss2.php' );					
+					//load_template( dirname(__FILE__) . "/mediatags_rss2.php");
+
+					$fname_parts = pathinfo(MEDIA_TAGS_RSS_TEMPLATE);
+					if (strlen($fname_parts['filename']))
+					{
+						$template_filename = TEMPLATEPATH. "/" . 
 							$fname_parts['filename'] . "-". $mediatag_term['term_id'] . 
 							".". $fname_parts['extension'];
 					
-					if ( !file_exists($template_filename) )
-						$template_filename = "";						
+						if ( !file_exists($template_filename) )
+						{
+							$template_filename = "";
+							$plugindir_node = dirname(__FILE__);	
+							$template_filename = $plugindir_node ."/".MEDIA_TAGS_RSS_TEMPLATE;
+						}
+					}
+					//echo "template_filename[".$template_filename."]<br />";
+					//include($template_filename);
+					load_template($template_filename);
+					exit;
+				}
+				else
+				{
+					$fname_parts = pathinfo(MEDIA_TAGS_TEMPLATE);
+					if (strlen($fname_parts['filename']))
+					{
+						$template_filename = TEMPLATEPATH. "/" . 
+							$fname_parts['filename'] . "-". $mediatag_term['term_id'] . 
+							".". $fname_parts['extension'];
+					
+						if ( !file_exists($template_filename) )
+							$template_filename = "";						
+					}
 				}
 			}
 		}
@@ -105,6 +142,8 @@ function mediatags_postsWhere($where)
 { 
 	global $wpdb;
 	
+	$whichmediatags	= "";
+	
 	//echo "_REQUEST[mediatag_id]=[".$_REQUEST['mediatag_id']."]<br />";
 	//echo "where - initial =[".$where."]<br />";
 	
@@ -116,11 +155,11 @@ function mediatags_postsWhere($where)
 		//echo "sermedia_tags_chk<pre>"; print_r($sermedia_tags_chk); echo "</pre>";
 
 		// Dear Wordpress. I hate parsing SQL. Find a better interface for this crap!
-		$where = str_replace("AND wp_posts.post_type = 'post'", "AND wp_posts.post_type = 'attachment'", $where);
-		$where = str_replace("(wp_posts.post_status = 'publish' OR wp_posts.post_status = 'private')", 
-								"(wp_posts.post_status = 'inherit')", $where);
-		$where = str_replace("(wp_posts.post_status = 'publish')", 
-								"(wp_posts.post_status = 'inherit')", $where);
+		$where = str_replace("AND $wpdb->posts.post_type = 'post'", "AND $wpdb->posts.post_type = 'attachment'", $where);
+		$where = str_replace("($wpdb->posts.post_status = 'publish' OR $wpdb->posts.post_status = 'private')", 
+								"($wpdb->posts.post_status = 'inherit')", $where);
+		$where = str_replace("($wpdb->posts.post_status = 'publish')", 
+								"($wpdb->posts.post_status = 'inherit')", $where);
 
 		//$token = "'" . MEDIA_TAGS_QUERYVAR . "'";
 		//echo "token=[".$token."]<br />";
@@ -167,4 +206,11 @@ function mediatags_postsJoin($join)
 	return $join;	
 }
 
+function mediatags_term_link($termlink, $term)
+{
+	if ($term->taxonomy == MEDIA_TAGS_TAXONOMY)
+		$termlink = get_mediatag_link($term->term_id);
+	
+	return $termlink;
+}
 ?>
